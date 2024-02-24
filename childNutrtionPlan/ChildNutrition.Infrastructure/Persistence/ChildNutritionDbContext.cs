1f2b48731dfcfef27d9bcb52b31436e2682138b6
im-Om123 <ombajracharya4@gmail.com>
2024-02-24 15:26:23 +0545

    first commit

---
+++ childNutrtionPlan/ChildNutrition.Infrastructure/Persistence/ChildNutritionDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ChildNutrition.Application.Interfaces;
+using ChildNutrition.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChildNutrition.Infrastructure.Persistence
+{
+    public class ChildNutritionDbContext : IdentityDbContext<User>, IChildNutritionDbContext
+    {
+        public ChildNutritionDbContext(DbContextOptions<ChildNutritionDbContext> options) : base(options) { }
+    }
+}
